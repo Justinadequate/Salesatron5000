c50f94ce9950da829396852ad956f95bcc4dd4e2
Nathan Norton <nathan.norton@loyalhealth.com>
2023-05-04 09:21:26 -0500

    Added slack bolt boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Salesatron5000
+
+## Enviroment
+Create your own `.env` file using the keys defined in the example: `.env-example`
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+const { App } = require('@slack/bolt');
 
-console.log("hello world!");
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();

dc567ee28af60b6b26e74307bb53867a454f2c2d
Nathan Norton <nathan.norton@loyalhealth.com>
2023-05-04 12:44:30 -0500

    logs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 require('dotenv').config()
 const { App } = require('@slack/bolt');
 
+console.log(process.env.SLACK_BOT_TOKEN);
+console.log(process.env.SLACK_SIGNING_SECRET);
+
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
+  logLevel: 'INFO',
+  // socketMode: true,
+  // appToken: process.env.SLACK_APP_TOKEN,
 });
 
 app.message(async ({message, say}) => {
   console.log(message);
   say('I GOT YO MESSAGE?');
 });
+
+
 
 (async () => {
   // Start the app

4b4a1429428b6294fdbec6911bb4ff1d03ea0bdd
Nathan Norton <nathan.norton@loyalhealth.com>
2023-05-04 14:04:34 -0500

    connecting to calendar

--- src/index.js
+++ src/index.js
@@ -1,27 +1,141 @@
-require('dotenv').config()
-const { App } = require('@slack/bolt');
+require("dotenv").config();
+const { App } = require("@slack/bolt");
 
-console.log(process.env.SLACK_BOT_TOKEN);
-console.log(process.env.SLACK_SIGNING_SECRET);
+const fs = require("fs").promises;
+const path = require("path");
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listEvents(auth) {
+  const calendar = google.calendar({ version: "v3", auth });
+  // const res = await calendar.calendars();
+  // const res = await calendar.calendarList.list({});
+  const res = await calendar.calendars.get({
+    calendarId: "c_f4bad3838ed561bf803618c1fa21c36bee072eba934ef29b3fb12e80ec2aec17@group.calendar.google.com",
+  });
+  console.log(res.data);
+
+  const ires = await calendar.events.insert({
+    calendarId: "c_f4bad3838ed561bf803618c1fa21c36bee072eba934ef29b3fb12e80ec2aec17@group.calendar.google.com",
+    requestBody: {
+      summary: "SALES DEMO",
+      description: "Yo!  A demo is happening",
+      start: {
+        dateTime: "2023-05-04T05:00:00-06:00",
+      },
+      end: {
+        dateTime: "2023-05-04T06:00:00-06:00",
+      },
+    },
+  });
+
+  // const res = await calendar.events.list({
+  //   calendarId: 'primary',
+  //   timeMin: new Date().toISOString(),
+  //   maxResults: 10,
+  //   singleEvents: true,
+  //   orderBy: 'startTime',
+  // });
+  // const events = res.data.items;
+  // if (!events || events.length === 0) {
+  //   console.log('No upcoming events found.');
+  //   return;
+  // }
+  // console.log('Upcoming 10 events:');
+  // events.map((event, i) => {
+  //   const start = event.start.dateTime || event.start.date;
+  //   console.log(`${start} - ${event.summary}`);
+  // });
+}
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
-  logLevel: 'INFO',
+  logLevel: "INFO",
   // socketMode: true,
   // appToken: process.env.SLACK_APP_TOKEN,
 });
 
-app.message(async ({message, say}) => {
+app.message(async ({ message, say }) => {
   console.log(message);
-  say('I GOT YO MESSAGE?');
+  say("I GOT YO MESSAGE?");
 });
-
-
 
 (async () => {
   // Start the app
-  await app.start(process.env.PORT || 3000);
+  try {
+    await app.start(process.env.PORT || 3001);
+    const auth = await authorize();
+    listEvents(auth);
+  } catch (e) {
+    console.error(e);
+  }
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();

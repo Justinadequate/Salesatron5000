ec5d0496dde3a6aca7c401ddcf47399db6949bba
Nathan Norton <nathan.norton@loyalhealth.com>
2023-05-05 12:16:51 -0500

    ageta

--- src/test.js
+++ src/test.js
@@ -6,3 +6,4 @@ const {extractDate, getDateTimeInfoFromRegex} = require('./date-extract');
 extractDate('may 11 12 pm');
 extractDate('may 1st 2pm')
 extractDate('today at 5pm');
+extractDate('I NEEDS A DEMO 5/6 11:23 PM');

2673ad12bd669bd54591ffcc0eba04adcc9389c1
Justinadequate <56033937+Justinadequate@users.noreply.github.com>
2023-05-05 11:20:14 -0400

    fix year identification

--- src/date-extract.js
+++ src/date-extract.js
@@ -1,6 +1,6 @@
 const { DateTime } = require('luxon');
 
-const DATE_TIME_REGEX = /\b(?<Month>\d{1,2})[-/](?<Day>\d{1,2})[-/]?(?<Year>\d{0,4})\b|\b(?<Month2>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*[.,]?\s+(?<Day2>\d{1,2}[a-z]{0,2}),?\s+(?<Year2>\d{0,4})\b|\b(?<Hour>1[012]|[1-9])(?!\/)(?!:(?=1(?!\s)))(?::(?<Minute>\d{2}))?\s?(?<AmPm>am|pm|AM|PM)?\b/gm;
+const DATE_TIME_REGEX = /\b(?<Month>\d{1,2})[-/](?<Day>\d{1,2})[-/]?(?<Year>\d{0,4})\b|\b(?<Month2>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*[.,]?\s+(?<Day2>\d{1,2}[a-z]{0,2}),?\s+(?<Year2>\d{2,4})?\b|\b(?<Hour>1[012]|[1-9])(?!\/)(?!:(?=1(?!\s)))(?::(?<Minute>\d{2}))?\s?(?<AmPm>am|pm|AM|PM)?\b/gm;
 
 function extractDate(message) {
   const groomedMessage = groomMessage(message);
@@ -33,10 +33,7 @@ const getDateTimeInfoFromRegex = (matches) => {
   return {
     day: matches[0].groups.Day ?? matches[0].groups.Day2,
     month: matches[0].groups.Month ?? matches[0].groups.Month2,
-    year:
-      matches[0].groups.Year ?? matches[0].groups.Year2.trim().length === 0
-        ? matches[0].groups.Year2
-        : new Date().getFullYear(),
+    year: matches[0].groups.Year ?? matches[0].groups.Year2 ?? new Date().getFullYear(),
     hour: matches[1]?.groups.Hour ?? 0,
     minute: matches[1]?.groups.Minute ?? 0,
     amPm: matches[1]?.groups.AmPm,
